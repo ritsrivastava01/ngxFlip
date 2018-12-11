133752de43c26bf816ea9318734e0ac0e749f929
hakimio <sandshrew@gmail.com>
2018-12-11 10:05:24 +0100

    Remove unused variable in demo app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngxflip-app';
   flip = false;
   rotate() {
     this.flip = !this.flip;
